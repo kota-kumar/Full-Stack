998a832ec2d783dd404cfe19d799be27bd373dfa
kota-kumar <kotasaikumar.ksk444@gmail.com>
2025-06-07 21:47:33 +0530

    push

---
+++ Projects/React/vite-project/components/MoviesCards.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import MoviesSection from './MoviesSection'
+function MoviesCards() {
+    const [moviecards, setmoviecards] = useState([])
+    useEffect(()=>{
+        axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=d9f6bfde6fe1971952f9beb3f97785ee&language=en-US&page=1`).then((Response)=>{
+            setmoviecards(Response.data.results)
+        })
+    },)
+  return (
+    <div>
+       <div className='space-y-8 flex flex-wrap gap-5 justify-evenly'>
+        {
+      moviecards.map((movieObj)=>(
+            <MoviesSection  movieObj={movieObj} />
+           
+      ))
+    
+    }
+ 
+      </div>
+    </div>
+  )
+}
+
+export default MoviesCards
--- Projects/React/vite-project/components/MoviesSection.jsx
+++ Projects/React/vite-project/components/MoviesSection.jsx
@@ -4,7 +4,7 @@ function MoviesSection() {
   return (
    <>
 
-    <div className='w-[200px] h-[60vh] bg-cover bg-center ml-8 rounded-lg' style={{backgroundImage:'URL(https://img.freepik.com/premium-psd/movie-poster_841014-31866.jpg?w=2000)'}}></div>
+    <div className='w-[200px] h-[50vh] bg-cover bg-center ml-8 rounded-lg' style={{backgroundImage:'URL(https://img.freepik.com/premium-psd/movie-poster_841014-31866.jpg?w=2000)'}}></div>
     </>
   )
 }
